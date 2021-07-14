8504121641363a5d5dfb818cf66a270e08ef137c
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-07-14 19:40:58 +0200

    adding a home page

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -8,9 +8,15 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
+    return render_template('home.html', title='Home')
+    
+
+@main.route("/chat")
+def chat():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    return render_template('chat.html', posts=posts)
+
 
 
 @main.route("/about")
